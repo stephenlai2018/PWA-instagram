dd0b31eb1891ae17af974d10589877786f9c3bb8
Stephen ChaoWen Lai <stephenlai2015@gmail.com>
2018-04-18 13:37:44 +0800

    1st commit
    restore feed.js

--- src/js/feed.js
+++ src/js/feed.js
@@ -2,34 +2,14 @@ var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 
-// once button is clicked, banner is prompted to ask if user wants to add the icon to home screen
-shareImageButton.addEventListener('click', openCreatePostModal);
-
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+}
 
 function closeCreatePostModal() {
   createPostArea.style.display = 'none';
 }
 
-// function openCreatePostModal() {
-//   createPostArea.style.display = 'block';
-//   if(deferredPrompt) {
-//     deferredPrompt.prompt();
-//     deferredPrompt.userChoice.then(choiceResult => {
-//       console.log(choiceResult.outcome);
+shareImageButton.addEventListener('click', openCreatePostModal);
 
-//       if(choiceResult.outcome === "dismissed") {
-//         console.log("User cancelled installation");
-//       }
-//       else {
-//         console.log("User added to home screen");
-//       }  
-//     });
-
-//     deferredPrompt = null;
-//   }
-// }
-
-
-
-
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
